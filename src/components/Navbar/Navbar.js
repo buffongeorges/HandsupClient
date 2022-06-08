f7dc6532927fff8c621a6c7228b1fa8e09118f4d
georges <georges.buffon@uha.fr>
2022-06-09 01:16:33 +0200

    fix build 2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../Button";
 import { MenuItems } from "./MenuItems";
 import "./Navbar.css";
